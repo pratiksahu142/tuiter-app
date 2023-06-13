c53c15c15bc3cd95b40c8aad76e400c73516ebbb
pratiksahu142 <pratik.sahu1421996@gmail.com>
2023-06-13 19:37:48 -0400

    ui padding fix 2

--- src/tuiter/who-to-follow-list/who-to-follow-list-item.js
+++ src/tuiter/who-to-follow-list/who-to-follow-list-item.js
@@ -7,20 +7,23 @@ const WhoToFollowListItem = (
 ) => {
   return (
       <li className="list-group-item">
-        <div className="row">
-          <div className="col-2">
-            <img className="rounded-circle" height={48}
-                 src={`/images/${who.avatarIcon}`}/></div>
-          <div className="col-7">
-            <div className="fw-bold ps-2">{who.userName}</div>
-            <div className=" ps-2">@{who.handle}</div>
+        <div className="d-flex align-items-center">
+          <div className="float-start">
+            <img className="rounded-circle" height={48} src={`/images/${who.avatarIcon}`} alt="Avatar" />
           </div>
-          <div className="col-3">
-            <button className="btn btn-primary rounded-pill float-end">Follow
-            </button>
+          <div className="d-inline-flex flex-grow-1">
+            <div className="ps-2">
+              <div className="fw-bold">{who.userName}</div>
+              <div>@{who.handle}</div>
+            </div>
+          </div>
+          <div className="float-end">
+            <button className="btn btn-primary rounded-pill btn-sm">Follow</button>
           </div>
         </div>
       </li>
+
+
   );
 };
 export default WhoToFollowListItem;
